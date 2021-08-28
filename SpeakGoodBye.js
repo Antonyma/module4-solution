74410bfbd387625f6d19615a147be99360c936e6
Antonio Angulo <antoniojesus184@gmail.com>
2021-08-28 19:12:26 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -17,7 +17,7 @@ var speakWord = "Good Bye";
 // See Lecture 52, part 2
 byeSpeaker.speak = function speak(name) {
   console.log(speakWord + " " + name);
-};
+}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
